088a4ada705541fb15994d3279635b4eb2693561
Jan Kowalke <jan.kowalke@eramon.de>
2025-06-02 16:13:39 +0200

    Revert "31179: set userTimezone"
    This reverts commit 673dd0f6eac4d856de6d935ea7fbb04d98637f69.

--- src/Jaspersoft/Service/JobService.php
+++ src/Jaspersoft/Service/JobService.php
@@ -25,13 +25,8 @@ class JobService
     private function makeUrl($params = null)
     {
         $url = $this->restUrl2 . '/jobs';
-        if (!empty($params)) {
+        if (!empty($params))
             $url .= '?' . Util::query_suffix($params);
-        }
-
-        $separator = (strpos($url, '?') !== false) ? '&' : '?';
-        $url .= $separator . 'userTimezone=' . date_default_timezone_get();
-
         return $url;
     }
 	
